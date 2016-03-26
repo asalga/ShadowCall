8314799f5092f9795ad39f6dea68e705d9368eb5
Andor Salga <andorsalga@gmail.com>
2016-03-26 19:51:48 -0400

    cleanup

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -2,50 +2,38 @@
 
 var Assets = require('../assets');
 var utils = require('../utils');
+var cfg = require('../config');
 
-var Game = function() {
-
-};
+var Game = function() {};
 
 /*
- 
- */
+*/
 Game.prototype = {
 
   create: function() {
     this.main = require('../main');
 
     this.tilesPerRow = 9;
-    this.totalRows = 150;
     this.visibleRows = 8;
-    this.speed = 1;
-
-    // create left and right vertical spacing
-    this.sideBuffer = 16;
-
-    // move this to model data?
+    this.tilesPerSecond = cfg.backgroundTilesPerSecond;
+    this.recycledIndices = [0, 1, 2, 3, 4, 5, 6, 7];
     this.TILE_SIZE = 32;
-
-
-    // track how far we have moved the board
-    this.pos = 0;
-
-    this.row = [];
-
-    var tile;
-
-    this.marker = this.visibleRows - 1;
+    this.sideBuffer = 16;   // create left and right vertical spacing
+    this.pos = 0;           // track how far we have moved the board
 
     // Makes working with the level a bit easier
     this._2dMap = [];
 
     // We have the level represented as a 1d array, we need to 
     // convert it into a 2d map
-    this.create2DMap(Assets.json.level1);
+    this.create2DMap(Assets.json.level2);
+
+    this.totalRows = this._2dMap.length;
 
     // create the sprites
     this.visibleTiles = [];
-
+    this.g = this.game.add.group();
+    
     // Create the board/grid. Use temp sprite texture
     for (var row = 0; row < this.visibleRows; row++) {
       this.visibleTiles.push([]);
@@ -53,15 +41,14 @@ Game.prototype = {
       for (var column = 0; column < this.tilesPerRow; column++) {
 
         var x = column * this.TILE_SIZE + this.sideBuffer;
-        var y = row * this.TILE_SIZE;
+        var y = row * this.TILE_SIZE - 32 -16 - 8;
         var nameIndex = '0001.png'; // temp
 
-        var test = this.game.add.sprite(x, y, 'level1', nameIndex);
-        this.visibleTiles[row].push(test);
-        test._posOrigY = y;
+        var sprite = this.game.add.sprite(x, y, 'level2', nameIndex);
+        this.g.add(sprite);
 
-        test.position._float_y = y;
-        test.position.y = Math.floor(test.position._float_y);
+        this.visibleTiles[row].push(sprite);
+        sprite.position.y = Math.floor(y);
       }
     }
 
@@ -69,7 +56,7 @@ Game.prototype = {
     // Here we assign the proper sprites to the tiles on the board.
     var startRow = this.totalRows - this.visibleRows;
     var boardRow = 0;
-
+    var tile;
     // This starts pointing to bottom of the 2d map and we work our way upwards
     this.nextRowMarker = this._2dMap.length - this.visibleRows;
 
@@ -84,7 +71,7 @@ Game.prototype = {
   /*
     Our level is represented as a large 1d array. We need to 
     place this in a 2d array for easier reading.
-  */
+    */
   create2DMap: function(_1dArray) {
     var arr = _1dArray.splice(0);
     while (arr.length) {
@@ -94,72 +81,66 @@ Game.prototype = {
 
   /*
     Given 1, returns 0001.png
-  */
+    */
   tileIndexToTileName: function(index) {
     return utils.addLeadingZeros(index, 4) + '.png';
   },
 
+  /*
+    Move the last row down
+    then iterate upwards moving each row down
+  */
   update: function() {
-    this.pos += this.speed;
+    this.pos += this.tilesPerSecond;
+    this.g.position.y = Math.floor(this.pos);
 
     var rows = this.visibleTiles.length;
     var cols = this.visibleTiles[0].length;
 
-    this.pos += this.speed;
+    var lastIndex = this.recycledIndices[this.recycledIndices.length - 1];
 
+    if (this.visibleTiles[lastIndex][0].position.y + this.g.position.y > 200) {
+      this.recycleRow(lastIndex);
+    }
+  },
+
+  debugDraw: function() {
+    var rows = this.visibleTiles.length;
+    var cols = this.visibleTiles[0].length;
     for (var r = 0; r < rows; r++) {
       for (var c = 0; c < cols; c++) {
-        // this.visibleTiles[r][c]._posOrigY += this.speed;
-        // var orig = this.visibleTiles[r][c]._posOrigY;
-        // this.visibleTiles[r][c].position.y = orig + Math.floor(this.testPos);
-        // ypos += this.speed;
-        this.visibleTiles[r][c].position._float_y += this.speed;
-        this.visibleTiles[r][c].position.y = Math.floor(this.visibleTiles[r][c].position._float_y);
-
-        // this.visibleTiles[r][c].position.y = Math.floor(this.visibleTiles[r][c]._posOrigY);
+        var s = this.visibleTiles[r][c];
+        this.game.debug.text(s.position.y + this.g.position.y, this.visibleTiles[r][c].position.x, this.visibleTiles[r][c].position.y);
       }
-    }
-
-    // 
-    var tile0 = this.visibleTiles[this.marker][0];
-    if (tile0.position.y >= (this.TILE_SIZE * this.visibleRows) - this.TILE_SIZE) {
-      this.recycleRow(this.marker);
     }
   },
 
   /*
-    Will change the y position and frame names of a given row from bottom
+    Changes the y position and frame names of a given row from bottom
     of the visible set of tiles to the very top.
-  */
+    */
   recycleRow: function(rowIndex) {
-
     this.nextRowMarker--;
 
-    for (var c = 0; c < this.tilesPerRow; c++) {
-
-      // First, lets move the bottom row to the top
-      this.visibleTiles[rowIndex][c].position._float_y = -this.TILE_SIZE + this.speed;
-      this.visibleTiles[rowIndex][c].position.y = -this.TILE_SIZE + this.speed;
+    for (var i = 0; i < this.tilesPerRow; i++) {
+      this.visibleTiles[rowIndex][i].position.y -= 8 * 32;
 
       // Second, assign new frames to the tiles
-      // var index = (this.nextRowMarker * this.tilesPerRow) + c;
-      var tileId = this._2dMap[this.nextRowMarker][c];
+      var index = (this.nextRowMarker * this.tilesPerRow) + i;
+      var tileId = this._2dMap[this.nextRowMarker][i];
 
-      this.visibleTiles[rowIndex][c].frameName = this.tileIndexToTileName(tileId);
+      this.visibleTiles[rowIndex][i].frameName = this.tileIndexToTileName(tileId);
     }
 
-    // 
-    this.marker--;
-    if (this.marker < 0) {
-      this.marker = this.visibleRows - 1;
-    }
+    this.recycledIndices.unshift(rowIndex);
+    this.recycledIndices.length = this.recycledIndices.length - 1;
   },
 
   render: function() {
     var col = '#FFFFFF';
     this.game.debug.text('fps:' + this.game.time.fps, 2, 15, col);
-    this.game.debug.text('y:' + this.pos, 2, 30, col);
-
+    this.game.debug.text('y:' + Math.floor(this.pos), 2, 30, col);
+    // this.debugDraw();
     this.main.blitPixiToCanvas();
   }
 };
